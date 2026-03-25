9ff48353231f32955d8c478ac45f63addfdb7500
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-03-25 19:21:40 +0530

    Path issue fixed

--- src/Hooks/UseProductCategory.jsx
+++ src/Hooks/UseProductCategory.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addCatgeoryProductsData } from "../App/ProductSlice";
+import { addCatgeoryProductsData } from "../app/ProductSlice";
 
 const UseProductCategory = (category) => {
   const dispatch = useDispatch();
