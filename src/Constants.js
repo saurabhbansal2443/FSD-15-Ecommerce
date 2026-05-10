1d2d2841ce2837f684516812814979b0f8a148c6
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-05-10 18:40:08 +0530

    update baseUrl to use production server

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8080/";
+export const baseUrl = "https://fsd15backend.onrender.com/";
 export const signupUrl = `${baseUrl}users/signup`;
 export const loginUrl = `${baseUrl}users/login`;
 export const getUser = `${baseUrl}users`
